6dd04262fa940271394df0cdb09a8068aac865d7
Tom <fletcher.tom@gmail.com>
2016-01-17 19:24:47 +0000

    Initial commit- working head tracking

---
+++ Assets/RotateWithVRHead.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using VR = UnityEngine.VR;
+
+public class RotateWithVRHead : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.localRotation = VR.InputTracking.GetLocalRotation(VR.VRNode.CenterEye);
+	}
+}
---
+++ Assets/Scripts/RotateWithObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateWithObject : MonoBehaviour {
+	public GameObject sourceObject;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 srcRot = sourceObject.transform.localRotation.eulerAngles;
+		srcRot.y = 0;
+		//srcRot.x *= -1;
+		this.transform.eulerAngles = srcRot;
+	}
+}
